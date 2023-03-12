839af179fef3f726d5669acf6eb2629386d87246
Sagarxox <109059306+Sagarxox@users.noreply.github.com>
2023-03-12 20:53:23 +0530

    [Sagar] Add: find float max number using CompareTo

---
+++ GenericsMaximumProgram/MaxCheck.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class MaximumCheck
+    {
+        public int MaximumInteger(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            throw new Exception("first,second and third are same");
+        }
+
+        public float MaximumFloat(float first, float second, float third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            throw new Exception("first , second and third are same");
+        }
+    }
+}
---
+++ GenericsMaximumProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            MaximumCheck fmax = new MaximumCheck();
+            float fresult = fmax.MaximumFloat(4.2f, 2.2f, 2.3f);
+            Console.WriteLine(fresult);
+
+            fresult = fmax.MaximumFloat(2.4f, 4.6f, 3.4f);
+            Console.WriteLine(fresult);
+
+            fresult = fmax.MaximumFloat(2.4f, 4.6f, 6.9f);
+            Console.WriteLine(fresult);
+        }
+    }
+}
